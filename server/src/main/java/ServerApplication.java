1cc9d076eb9f3daf66d40f854b6c6f81e555af96
cunho <ancunho@naver.com>
2022-01-25 04:35:55 +0800

    'git相关'

---
+++ server/src/main/java/ServerApplication.java
@@ -0,0 +1,10 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        log.info(">>>>>>>>>>>>>>>>>>>>>>>>>Server Applicaiton Started");
+    }
+
+}
