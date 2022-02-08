c018a500f7eba9d45be3b22b2906a51bae311894
cunho <ancunho@naver.com>
2022-02-08 21:05:29 +0800

    page-menu

--- front-admin/src/views/admin/system/page-menu.vue
+++ front-admin/src/views/admin/system/page-menu.vue
@@ -1,137 +1,144 @@
 <template>
-  <div>
-    <div class="page-header">
-      <h1>Menu<small><i class="ace-icon fa fa-angle-double-right"></i>XXXXXXX</small></h1>
-    </div><!-- /.page-header -->
+    <div v-loading="loading">
 
-    <div class="row">
-      <div class="col-xs-12">
-        <!-- PAGE CONTENT BEGINS -->
-        <div class="row">
-          <div class="col-xs-12">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary btn-sm" v-on:click="addNewMenuHandler()" data-toggle="modal" data-target="#myModal">
-              新增菜单
-            </button>
-            <br/><br/>
-            <table id="simple-table" class="table  table-bordered table-hover">
-              <thead>
-                <tr>
-                  <th class="center">No.</th>
-                  <th>Name.</th>
-                  <th>Path.</th>
-                  <th>Perms.</th>
-                  <th class="center">Status</th>
-                  <th class="center">Createtime</th>
-                  <th class="center">Updatetime</th>
-                  <th class="center">
-                    caozuo
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="menuItem in menuList">
-                  <td class="center">{{ menuItem.rn }}</td>
-                  <td>{{ menuItem.name }}</td>
-                  <td>{{ menuItem.path }}</td>
-                  <td>{{ menuItem.perms }}</td>
-                  <td class="center">
-                    <span class="badge badge-danger" v-if="menuItem.status == '0'">{{ menuItem.statusName }}</span>
-                    <span class="badge badge-success" v-if="menuItem.status == '1'">{{ menuItem.statusName }}</span>
-                  </td>
-                  <td class="center">{{ menuItem.createtime }}</td>
-                  <td class="center">{{ menuItem.updatetime }}</td>
-                  <td class="center">
-                    <button class="btn btn-xs btn-info" @click="modifyMenuHandler(menuItem)"><i class="ace-icon fa fa-pencil bigger-120"> 修改</i></button>
-                    &nbsp;&nbsp;
-                    <button class="btn btn-xs btn-danger"><i class="ace-icon fa fa-trash-o bigger-120"> 删除</i></button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div><!-- /.span -->
-        </div><!-- /.row -->
+        <el-button @click="addNewMenuHandler()" type="primary" icon="el-icon-plus">新增菜单</el-button>
 
-        <div class="hr hr-18 dotted hr-double"></div>
+        <!--  table list start  -->
+        <el-table :data="menuList" style="width: 100%; margin-top: 1.5rem;">
+            <el-table-column prop="rn" label="编号" width="80"></el-table-column>
+            <el-table-column prop="name" label="菜单名" width="150"></el-table-column>
+            <el-table-column prop="path" label="路径" width="150"></el-table-column>
+            <el-table-column prop="perms" label="权限code" width="250"></el-table-column>
+            <el-table-column prop="status" label="状态" align="center" width="120">
+                <template slot-scope="scope">
+                    <el-tag type="danger" v-if="scope.row.status == '0'"> {{ scope.row.statusName }}</el-tag>
+                    <el-tag type="success" v-if="scope.row.status == '1'"> {{ scope.row.statusName }}</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column prop="createtime" label="创建时间" width="220"></el-table-column>
+            <el-table-column prop="updatetime" label="更新时间" width="220"></el-table-column>
+            <el-table-column label="操作" align="center">
+                <template slot-scope="scope">
+                    <el-button @click="modifyMenuHandler(scope.row)" type="primary" icon="el-icon-edit-outline">修改菜单</el-button>
+                    <!--          <el-button @click="getRoleHandler(scope.row)" type="danger" icon="el-icon-delete">删除</el-button>-->
+                </template>
+            </el-table-column>
+        </el-table>
+        <!--  // table list end  -->
 
-        <!-- PAGE CONTENT ENDS -->
-      </div><!-- /.col -->
-    </div><!-- /.row -->
+        <!--  paging start  -->
+        <el-row style="margin: 2rem 0;">
+            <el-col :span="12" :offset="6">
+                <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage"
+                    :page-sizes="pageSizes"
+                    :page-size="pageSize"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="total">
+                </el-pagination>
+            </el-col>
+        </el-row>
+        <!--  // paging end  -->
 
-    <div class="modal" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-          </div>
-          <div class="modal-body">
-            <div class="form-group">
-              <input type="hidden" v-model="modifyMenuItem.id" />
-              <p><span>id</span> <input type="text" disabled v-model="modifyMenuItem.id" /> </p>
-              <p><span>名称</span> <input type="text" v-model="modifyMenuItem.name" /> </p>
-              <p><span>perms</span> <input type="text" v-model="modifyMenuItem.perms" /> </p>
-              <p><span>type</span> <input type="text" v-model="modifyMenuItem.type" /> </p>
-              <p><span>status</span>  <input type="text" v-model="modifyMenuItem.status" /> </p>
+        <!--  dialog start  -->
+        <el-dialog v-bind:title="buzaModalTitle" :visible.sync="isModalVisible" :close-on-click-modal="false">
+            <!--:close-on-click-modal="false"-->
+            <input type="hidden" v-model="modifyMenuItem.id"/>
+            <el-form ref="form" label-width="80px">
+                <el-form-item label="ID" v-if="modifyMenuItem.id != null">
+                    <el-input v-model="modifyMenuItem.id" disabled></el-input>
+                </el-form-item>
+                <el-form-item label="名称">
+                    <el-input v-model="modifyMenuItem.name"></el-input>
+                </el-form-item>
+                <el-form-item label="Perms">
+                    <el-input v-model="modifyMenuItem.perms"></el-input>
+                </el-form-item>
+                <el-form-item label="type">
+                    <el-input v-model="modifyMenuItem.type"></el-input>
+                </el-form-item>
+                <el-form-item label="是否激活">
+                    <el-switch v-model="modifyMenuItem.status"></el-switch>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button type="danger" v-on:click="isModalVisible = false">取 消</el-button>
+                <el-button type="primary" v-on:click="saveMenu(modifyMenuItem)">保存用户</el-button>
             </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary" v-on:click="saveMenu(modifyMenuItem)">Save changes</button>
-          </div>
-        </div>
-      </div>
+        </el-dialog>
+        <!--  // dialog end  -->
+
     </div>
-
-
-
-
-  </div>
 </template>
 
 <script>
 
 export default {
-  name: "page-menu",
-  data: function() {
-    return {
-      menuList: [],
-      modifyMenuItem: {}
-    }
-  },
-  mounted: function() {
-    let _this = this;
-    _this.list();
-    $.getScript("/ace/assets/js/bootbox.js");
-  },
-  methods: {
-    list() {
-      let _this = this;
-      _this.$request.get(process.env.VUE_APP_SERVER + "/system/config/menu/list").then((response) => {
-        _this.menuList = response.data.data;
-      })
-    },
-    addNewMenuHandler() {
-      let _this = this;
-      _this.modifyMenuItem = {};
-    },
-    modifyMenuHandler(menuItem) {
-      let _this = this;
-      $("#myModal").modal('show');
-      _this.modifyMenuItem = menuItem;
-    },
-    saveMenu(sysMenuDto) {
-      let _this = this;
-      _this.$request.post(process.env.VUE_APP_SERVER + "/system/config/menu/modify", sysMenuDto).then(response => {
-        if (response.data.status == 200) {
-          _this.list();
-        } else {
-          alert(response.data.msg);
+    name: "page-menu",
+    data: function () {
+        return {
+            menuList: [],
+            modifyMenuItem: {},
+            loading: true,
+            currentPage: 1, //page
+            pageSize: 5, //limit
+            pageSizes: [5, 15, 30, 50],
+            total: 100,
+            isModalVisible: false,
+            buzaModalTitle: 'Modal',
         }
-      });
+    },
+    mounted: function () {
+        let _this = this;
+        _this.listTable();
+    },
+    methods: {
+        handleSizeChange(limit) {
+            this.currentPage = 1;
+            this.pageSize = limit;
+            this.listTable();
+        },
+        handleCurrentChange(page) {
+            this.currentPage = page;
+            this.listTable();
+        },
+        listTable() {
+            let _this = this;
+            _this.$request.get(process.env.VUE_APP_SERVER + "/system/config/menu/list?page=" + _this.currentPage + "&limit=" + _this.pageSize).then((response) => {
+                _this.menuList = response.data.data;
+                _this.total = response.data.count;
+                _this.loading = false;
+            })
+        },
+        addNewMenuHandler() {
+            let _this = this;
+            _this.isModalVisible = true;
+            _this.buzaModalTitle = "新增菜单";
+            _this.modifyMenuItem = {};
+        },
+        modifyMenuHandler(menuItem) {
+            let _this = this;
+            _this.isModalVisible = true;
+            _this.buzaModalTitle = "编辑菜单";
+            menuItem.status = menuItem.status == "1" ? true : false;
+            _this.modifyMenuItem = menuItem;
+        },
+        saveMenu(sysMenuDto) {
+            let _this = this;
+            sysMenuDto.status = sysMenuDto.status === true ? "1" : "0";
+            _this.$request.post(process.env.VUE_APP_SERVER + "/system/config/menu/modify", sysMenuDto).then(response => {
+                if (response.data.status == 200) {
+                    _this.listTable();
+                } else {
+                    alert(response.data.msg);
+                }
+                _this.isModalVisible = false;
+            });
 
+        }
     }
-  }
 }
 </script>
 
