7de309b3b52fb001643d5fa15be4f9619635a8f1
cunho <ancunho@naver.com>
2022-02-24 13:59:39 +0800

    sku service

--- server/src/main/java/com/buza/server/service/SkuService.java
+++ server/src/main/java/com/buza/server/service/SkuService.java
@@ -1,4 +1,55 @@
 package com.buza.server.service;
 
+import com.buza.server.dto.*;
+import com.buza.server.entity.*;
+
+import java.util.List;
+
 public interface SkuService {
+
+    // TB_SPU
+    public List<TbSpuDto> lstTbSpu(TbSpuDto tbSpuDto);
+    public TbSpuDto getTbSpuInfoBySpuId(Integer spuId);
+    public Boolean insertTbSpu(TbSpu tbSpu);
+    public Boolean updateTbSpu(TbSpu tbSpu);
+
+    // TB_SKU
+    public List<TbSkuDto> lstTbSku(TbSkuDto tbSkuDto);
+    public TbSkuDto getTbSkuInfoBySkuId(Integer skuId);
+    public List<TbSkuDto> lstTbSkuBySpuId(Integer spuId);
+    public Boolean insertTbSku(TbSku tbSku);
+    public Boolean updateTbSku(TbSku tbSku);
+
+    // TB_SPU_ATTRIBUTE
+    public List<TbSpuAttributeDto> lstTbSpuAttribute(TbSpuAttributeDto tbSpuAttributeDto);
+    public TbSpuAttributeDto getTbSpuAttributeInfoBySpuAttrId(Integer spuAttrId);
+    public List<TbSpuAttributeDto> lstTbSpuAttributeBySpuId(Integer spuId);
+    public List<TbSpuAttributeDto> lstTbSpuAttributeByAttrId(Integer attrId);
+    public Boolean insertTbSpuAttribute(TbSpuAttribute tbSpuAttribute);
+    public Boolean updateTbSpuAttribute(TbSpuAttribute tbSpuAttribute);
+
+    // TB_SKU_ATTRIBUTE
+    public List<TbSkuAttributeDto> lstTbSkuAttribute(TbSkuAttributeDto tbSkuAttributeDto);
+    public TbSkuAttributeDto getTbSkuAttributeBySkuAttrId(Integer skuAttrId);
+    public List<TbSkuAttributeDto> lstTbSkuAttributeBySkuId(Integer skuId);
+    public List<TbSkuAttributeDto> lstTbSkuAttributeByAttrValueId(Integer attrValueId);
+    public Boolean insertTbSkuAttribute(TbSkuAttribute tbSkuAttribute);
+    public Boolean updateTbSkuAttribute(TbSkuAttribute tbSkuAttribute);
+
+    // TB_ATTRIBUTE
+    public List<TbAttributeDto> lstTbAttribute(TbAttributeDto tbAttributeDto);
+    public TbAttributeDto getTbAttributeInfoByAttrId(Integer attrId);
+    public Boolean insertTbAttribute(TbAttribute tbAttribute);
+    public Boolean updateTbAttribute(TbAttribute tbAttribute);
+
+    // TB_ATTRIBUTE_VALUE
+    public List<TbAttributeValueDto> lstTbAttributeValue(TbAttributeValueDto tbAttributeValueDto);
+    public TbAttributeValueDto getTbAttributeValueInfoByAttrValueId(Integer attrValueId);
+    public Boolean insertTbAttributeValue(TbAttributeValue tbAttributeValue);
+    public Boolean updateTbAttributeValue(TbAttributeValue tbAttributeValue);
+
+
+
+
+
 }
